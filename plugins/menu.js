89848f3476739503fff799fc9c6160acf350c765
alinafis0 <160432216+alinafis0@users.noreply.github.com>
2024-03-01 13:23:13 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -28,7 +28,7 @@ const defaultMenu = {
 ⏰ *Uptime:* %muptime  
 ┗━━━━━━━━━━⬣
 %readmore
-  ≡ *B O B I Z A | M E N U*
+  ≡ *N A F I S | M E N U*
 `.trimStart(),
   header: '┏━━⊜ *_%category_* ',
   body: '┃⋄ %cmd %isdiamond %isPremium',
