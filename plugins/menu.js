c4af1972a7b25e9a8271cd9e46d036d308e7a6c3
alinafis0 <160432216+alinafis0@users.noreply.github.com>
2024-03-02 12:31:48 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -138,7 +138,7 @@ contextInfo: {
 externalAdReply: {
 title: 'NAFIS BOT ♥',
 body: "أول بوت واتساب في العالم العربي 💖",
-thumbnailUrl: 'https://telegra.ph/file/2829c7653514416d207e2.jpg',
+thumbnailUrl: 'https://telegra.ph/file/6299e3c003d04898fae39.mp4',
 sourceUrl: 'https://instagram.com/alinafis0',
 mediaType: 1,
 renderLargerThumbnail: true
